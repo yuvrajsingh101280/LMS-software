50065f60e4b0647e4949ec8cb3e326bc576d1648
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-03-31 16:11:02 +0530

    Add useParams and useState hooks to CourseDetails component for course data management

--- frontend/src/pages/students/CourseDetails.jsx
+++ frontend/src/pages/students/CourseDetails.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const CourseDetails = () => {
+  const { id } = useParams();
+  const [courseData, setCourseData] = useState(null);
   return <div></div>;
 };
 
