7cdf890d735cbe18c1209acbf0fcf2caf9cb65c0
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-04-30 12:29:06 +0530

    Add Footer component with copyright and social media links; update Educator page to include Footer

--- frontend/src/components/educator/Footer.jsx
+++ frontend/src/components/educator/Footer.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { assets } from "../../assets/assets";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className="flex md:flex-row flex-col-reverse justify-between border-t-2 border-gray-600/80 items-center">
+      <div className="flex items-center gap-4">
+        <img src={assets.logo} alt="" className="hidden md:block w-20" />
+        <div className="hidden md:block h-7 w-px bg-gray-500/60"></div>
+        <p className="py-4 text-center text-xs md:text-sm text-gray-500">
+          Copyright 2025 © Academix. All Right Reserved
+        </p>
+      </div>
+
+      <div className="flex flex-center gap-3 max-md:mt-4">
+        <a href="#">
+          <img src={assets.facebook_icon} alt="" />
+        </a>
+        <a href="#">
+          <img src={assets.twitter_icon} alt="" />
+        </a>
+        <a href="#">
+          <img src={assets.instagram_icon} alt="" />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- frontend/src/pages/educator/Educator.jsx
+++ frontend/src/pages/educator/Educator.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/educator/Navbar";
 import Sidebar from "../../components/educator/Sidebar";
+import Footer from "../../components/educator/Footer";
 const Educator = () => {
   return (
     <div className="text-default min-h-screen bg-white">
@@ -12,6 +13,7 @@ const Educator = () => {
           <Outlet />
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
