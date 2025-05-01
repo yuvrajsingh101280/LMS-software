43efaed4ebc4fd503447e04fd396375cf411adff
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-05-01 14:42:44 +0530

    Refactor AddCourse component to include Quill editor and unique ID generation

--- frontend/src/pages/educator/AddCourse.jsx
+++ frontend/src/pages/educator/AddCourse.jsx
@@ -1,6 +1,11 @@
-import React from "react";
-
+import React, { useRef } from "react";
+import uniqid from "uniqid";
+import Quill from "quill";
 const AddCourse = () => {
+  const quillRef = useRef(null);
+
+  const editorRed = useRef(null);
+
   return <div>Add course</div>;
 };
 
