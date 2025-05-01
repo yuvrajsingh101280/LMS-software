e3dbe823077a6e73bbe885526caa7cd22ec62186
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-05-01 11:18:05 +0530

    Implement MyCourses page with course fetching and display; add loading state and table layout

--- frontend/src/pages/educator/AddCourse.jsx
+++ frontend/src/pages/educator/AddCourse.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import Navbar from "../../components/educator/Navbar";
 
 const AddCourse = () => {
-  return <div>Add course </div>;
+  return <div>Add course</div>;
 };
 
 export default AddCourse;
--- frontend/src/pages/educator/MyCourses.jsx
+++ frontend/src/pages/educator/MyCourses.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import Navbar from "../../components/educator/Navbar";
+import { Appcontext } from "../../context/AppContext";
+import Loading from "../../components/students/Loading";
 
 const MyCourses = () => {
-  return <div>My course</div>;
+  const { currency, allCourses } = useContext(Appcontext);
+
+  const [courses, setCourses] = useState(null);
+
+  const fetchEducatorcourses = async () => {
+    setCourses(allCourses);
+  };
+
+  useEffect(() => {
+    fetchEducatorcourses();
+  }, [courses]);
+
+  return courses ? (
+    <div className="h-screen flex  flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0">
+      <div className="w-full">
+        <h2 className="pb-4 text-lg font-medium">My courses</h2>
+
+        <div className="flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-500/20">
+          <table className="md:table-auto table-fixed w-full overflow-hidden">
+            <thead className="text-gray-900 border-b border-gray-500/20 text-sm text-left">
+              <tr>
+                <th className="px-4 py-3 font-semibold truncate">
+                  All Courses
+                </th>
+                <th className="px-4 py-3 font-semibold truncate">Earnings</th>
+                <th className="px-4 py-3 font-semibold truncate">Students</th>
+                <th className="px-4 py-3 font-semibold truncate">
+                  Published On
+                </th>
+              </tr>
+            </thead>
+
+            <tbody className="text-sm text-gray-500">
+              {courses.map((course) => (
+                <tr key={course._id} className="border-b border-gray-500/20">
+                  <td className="md:px-4 pl-2 md:pl-4 py-3 flex items-center space-x-3  truncate">
+                    <img src={course.courseThumbnail} alt="" className="w-16" />
+                    <span className="truncate hidden md:block">
+                      {course.courseTitle}
+                    </span>
+                  </td>
+                  <td className="px-4 py-3">
+                    {currency}
+                    {(
+                      course.enrolledStudents.length *
+                      (course.coursePrice -
+                        (course.discount * course.coursePrice) / 100)
+                    ).toFixed(2)}
+                  </td>
+                  <td className="px-4 py-3">
+                    {course.enrolledStudents.length}
+                  </td>
+                  <td className="px-4 py-3">
+                    {new Date(course.createdAt).toLocaleDateString()}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Loading />
+  );
 };
 
 export default MyCourses;
