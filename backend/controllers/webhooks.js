84ed0391fdfec7628018fd370c320ac2fc8bc102
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-05-07 14:47:51 +0530

    Fix email address extraction in user creation from webhook data

--- backend/controllers/webhooks.js
+++ backend/controllers/webhooks.js
@@ -28,7 +28,7 @@ export const clerkWebhook = async (req, res) => {
                 const userData = {
 
                     _id: data.id,
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
 
