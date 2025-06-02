98fd5773e20c268c3a38fd39bda8fe2af4766bed
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-06-03 01:21:47 +0530

    bb

--- backend/controllers/webhook.js
+++ backend/controllers/webhook.js
@@ -4,9 +4,15 @@ const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 export const stripeWebhooks = async (req, res) => {
 
+    const sig = request.headers['stripe-signature'];
 
+    let event;
 
-
-
+    try {
+        event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    }
+    catch (err) {
+        response.status(400).send(`Webhook Error: ${err.message}`);
+    }
 
 }
