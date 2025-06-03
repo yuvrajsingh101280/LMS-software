501e99ad0d5a6a8c5e7ad8e6d086f44027af80b4
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-06-03 14:02:43 +0530

    a

--- backend/index.js
+++ backend/index.js
@@ -20,14 +20,14 @@ await connectToDatabase();
 await connectCloudinary();
 
 // Stripe webhook route - must be before express.json()
-app.post("/stripe", express.json({ type: 'application/json' }), stripeWebhooks);
+app.post("/stripe", express.raw({ type: 'application/json' }), stripeWebhooks);
 
 // Middlewares
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }));
-app.use(express.json()); // after /stripe
+// app.use(express.json()); // after /stripe
 app.use(cookieParser());
 
 // Routes
@@ -35,10 +35,10 @@ app.get("/", (req, res) => {
     res.send("API is working");
 });
 
-app.use("/api/auth", authRouter);
-app.use("/api/educator", educatorRouter);
-app.use("/api/course", courseRouter);
-app.use("/api/user", userRouter);
+app.use("/api/auth", express.json(), authRouter);
+app.use("/api/educator", express.json(), educatorRouter);
+app.use("/api/course", express.json(), courseRouter);
+app.use("/api/user", express.json(), userRouter);
 
 // Optional: Error handler
 app.use((err, req, res, next) => {
